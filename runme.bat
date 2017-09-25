a564ca33e967caa4c2ddef1f47df0080857ddc8c
Christopher Stock <christopher.stock@ergodirekt.de>
2017-09-25 16:04:36 +0200

    Setup initial springboot primer and test suite.

---
+++ runme.bat
@@ -0,0 +1,1 @@
+mvn package && java -jar target\SpringBootPrimer-1.0.0.jar
---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,37 @@
+
+    package hello;
+
+    import  java.util.Arrays;
+    import  org.springframework.boot.*;
+    import  org.springframework.boot.autoconfigure.*;
+    import  org.springframework.context.*;
+    import  org.springframework.context.annotation.*;
+
+    @SpringBootApplication
+    public class Application
+    {
+        /***************************************************************************************************************
+        *   TODO Colorize and create IJ Module.
+        ***************************************************************************************************************/
+        public static void main( String[] args )
+        {
+            SpringApplication.run( Application.class, args );
+        }
+
+        @Bean
+        public CommandLineRunner commandLineRunner( ApplicationContext ctx )
+        {
+            return args -> {
+
+                System.out.println( "Here are the beans provided by Spring Boot:" );
+
+                String[] beanNames = ctx.getBeanDefinitionNames();
+                Arrays.sort( beanNames );
+
+                for ( String beanName : beanNames )
+                {
+                    System.out.println( beanName );
+                }
+            };
+        }
+    }
